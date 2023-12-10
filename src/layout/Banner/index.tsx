40f32c29794e5785b069d944810425143384cfc9
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2023-12-10 08:02:28 +0700

    refactor banner section and styling

--- src/layout/Banner/index.tsx
+++ src/layout/Banner/index.tsx
@@ -1,21 +1,26 @@
 import "./index.css"
 import image from "../../assets/banner-illustration-01.webp"
+import backImage from "../../assets/banner-background-01.webp"
 import { Button } from "@mui/material";
+import { Navbar } from "..";
 
 const Banner = () => {
 
 
     return (
         <>
+            <img className="back-image" src={backImage} alt="" />
             <div className="banner-container">
-                <div>
-                    <p>Elevate Your Well-being:</p>
-                    <p>Navigating Health, One Click at a Time!</p>
+                <Navbar/>
+                <div className="banner-asset">
+                    <div>
+                        <p className="banner-title"><span className="tag-title">Elevate Your Well-being:</span><br />Navigating Health, One Click at a Time!</p>
+                    </div>
+                    <div >
+                        <img className="image" src={image} alt="" />
+                    </div>
+                    <Button className="banner-button" variant="contained">Start Your Wellness Journey {'>>>'}</Button>
                 </div>
-                <div >
-                    <img className="image" src={image} alt="" />
-                </div>
-                <Button variant="contained">Start Your Wellness Journey</Button>
             </div>
         </>
     )
