cd1f446307c9544cf2115f6c379b002eaa463b60
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2023-12-13 00:31:30 +0700

    foldering profile page

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as LoginForm } from "./LoginForm";
 export { default as KeyFeature } from "./KeyFeature";
 export { default as RegistrationForm } from "./RegistrationForm";
 export { default as GoogleAuthButton } from "./GoogleAuthButton";
+export { default as UserProfile } from "./UserProfile";
