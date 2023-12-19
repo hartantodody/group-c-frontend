d674c87b379afe8fde8521556cba069ac56fa785
renalidi <sumitomo.andi@gmail.com>
2023-12-19 22:55:37 +0700

    feat: styling sleep card

--- src/components/SleepMenu/index.tsx
+++ src/components/SleepMenu/index.tsx
@@ -1,70 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import Collapse from "@mui/material/Collapse";
-import TextField from "@mui/material/TextField";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider, DesktopDatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider, MobileDateTimePicker } from "@mui/x-date-pickers";
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
 import addDays from "date-fns/addDays";
+import { Box } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface SleepMenuProps {
   sleepStartProp?: string;
   wakeUpProp?: string;
 }
 
-const SleepMenu: React.FC<SleepMenuProps> = ({ sleepStartProp, wakeUpProp }) => {
-  const [sleepStart, setSleepStart] = useState<Date | null>(sleepStartProp ? new Date(sleepStartProp) : null);
-  const [wakeUp, setWakeUp] = useState<Date | null>(wakeUpProp ? new Date(wakeUpProp) : null);
-  const [collapsed, setCollapsed] = useState<boolean>(false);
+const SleepMenu: React.FC<SleepMenuProps> = () => {
+  const [expanded, setExpanded] = useState(false);
+  const [sleepStart, setSleepStart] = useState<Date | null>(null);
+  const [sleepEnd, setSleepEnd] = useState<Date | null>(null);
 
-  useEffect(() => {
-    // Update the state when props change
-    setSleepStart(sleepStartProp ? new Date(sleepStartProp) : null);
-    setWakeUp(wakeUpProp ? new Date(wakeUpProp) : null);
-  }, [sleepStartProp, wakeUpProp]);
+  const handleDatePickerSleepStart = (date: Date | null) => {
+    setSleepStart(date);
+  };
+  
+  const handleDatePickerSleepEnd = (date: Date | null) => {
+    setSleepEnd(date);
+  };
 
-  const handleToggleCollapse = () => {
-    setCollapsed((prevCollapsed) => !prevCollapsed);
+  const handleExpandClick = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
   };
 
   return (
     <div style={{ margin: "10px" }}>
-      <Button variant='text' color='primary' onClick={handleToggleCollapse}>
-        {collapsed ? "Expand Sleep" : "Collapse Sleep"}
-      </Button>
-      <Collapse in={!collapsed}>
-        <div style={{ marginTop: "10px" }}>
-          <Typography variant='h6'>Sleep</Typography>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <TextField fullWidth variant='outlined' style={{ marginTop: "10px" }}>
-              <DesktopDatePicker
-                label='Sleep Start'
-                value={sleepStart}
-                onChange={(date: Date | null) => setSleepStart(date)}
-              />
-            </TextField>
-            <TextField fullWidth variant='outlined' style={{ marginTop: "10px" }}>
-              <DesktopDatePicker label='Wake Up' value={wakeUp} onChange={(date: Date | null) => setWakeUp(date)} />
-            </TextField>
-          </LocalizationProvider>
-          <div style={{ marginTop: "10px" }}>
-            <Button
-              variant='outlined'
-              color='secondary'
-              onClick={() => setSleepStart((prevDate) => addDays(prevDate || new Date(), 1))}
+      <div style={{ marginTop: "10px" }}>        
+        <Typography variant='h5'>Sleep</Typography>
+        <AnimatePresence>
+          {expanded && (
+            <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
             >
-              Add a day to Sleep Start
-            </Button>
-            <Button
-              variant='outlined'
-              color='secondary'
-              onClick={() => setWakeUp((prevDate) => addDays(prevDate || new Date(), 1))}
-            >
-              Add a day to Wake Up
-            </Button>
-          </div>
-        </div>
-      </Collapse>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DemoContainer components={['DatePicker']}>
+                  <MobileDateTimePicker
+                    label="Sleep start time"
+                    format="yyyy-MM-dd hh:mm"
+                    value={sleepStart}
+                    onChange={handleDatePickerSleepStart}
+                  />
+                </DemoContainer>
+                <DemoContainer components={['DatePicker']}>
+                  <MobileDateTimePicker
+                    label="Wake up time"
+                    format="yyyy-MM-dd hh:mm"
+                    value={sleepEnd}
+                    onChange={handleDatePickerSleepEnd}
+                  />
+                </DemoContainer>
+              </LocalizationProvider>
+              <div style={{ marginTop: "10px" }}>
+                <Button
+                  variant='outlined'
+                  color='secondary'
+                  onClick={() => setSleepStart((prevDate) => addDays(prevDate || new Date(), 1))}
+                >
+                  submit
+                </Button>
+              </div>
+            </motion.div>
+          )}          
+        </AnimatePresence>
+      </div>
+      <Box style={{ marginTop: 5 }}>
+        <Button onClick={handleExpandClick} variant='outlined' color='secondary'>
+          {expanded ? "Collapse" : "Expand"}
+        </Button>
+      </Box>
     </div>
   );
 };
