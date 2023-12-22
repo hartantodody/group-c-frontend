54dcacc7b985c14f6ea11aab67479a6d033c7382
Dody Hartanto <muharridody@gmail.com>
2023-12-23 02:10:05 +0800

    hotfix: add back button

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,20 +1,23 @@
-import {UserProfile } from "../../components";
-import profileImage from "../../assets/ProfilePageAssets/Group 44.jpg"
-import "./index.css"
+import { UserProfile, BackButton } from "../../components";
+import profileImage from "../../assets/ProfilePageAssets/Group 44.jpg";
+import "./index.css";
 import { Navbar } from "../../layout";
 
 const ProfilePage = () => {
-    return (
-      <>
-        <div className="profile-container">
-            <Navbar/>
-            <img className="profile-image" src={profileImage} alt="" />
-            <div className="card">
-                <UserProfile/>
-            </div>
+  return (
+    <>
+      <div className='profile-container'>
+        <Navbar />
+        <img className='profile-image' src={profileImage} alt='' />
+        <div className='card'>
+          <div style={{ position: "absolute", top: 0, left: 0, margin: "10px 0 0 10px" }}>
+            <BackButton />
+          </div>
+          <UserProfile />
         </div>
-      </>
-    );
-  };
-  
+      </div>
+    </>
+  );
+};
+
 export default ProfilePage;
